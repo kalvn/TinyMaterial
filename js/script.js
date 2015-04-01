135b8f0fcb8f486b2a80a5e33c7349a135a948fe
kalvn <kalvnthereal@gmail.com>
2015-04-01 23:29:04 +0200

    Adds popups.

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@
         });
 
         // Ripple effect.
-        $('.btn, .btn-raised, a.list-item').not('[disabled]').on('click', function(event) {
+        $('.btn, .btn-raised, a.list-item').not('[disabled]').on('mousedown', function(event) {
             //event.preventDefault();
-            event.stopPropagation();
+            //event.stopPropagation();
             var $div = $('<div/>'),
                 btnOffset = $(this).offset(),
                 xPos = event.pageX - btnOffset.left,
@@ -28,9 +28,50 @@
                 background: $(this).data("ripple-color")
             }).appendTo($(this));
 
+            // $div.css({
+            //     top: yPos,
+            //     left: xPos,
+            //     background: $(this).data("ripple-color")
+            // }).appendTo($(this));
+
             window.setTimeout(function() {
                 $div.remove();
             }, 2000);
+        });
+
+        // Popups.
+        $('[data-popup]').on('click', function(event){
+            event.stopPropagation();
+
+            var el = $(this);
+            var popup = $(el.data('popup'));
+
+            if(popup.length > 0){
+                if(popup.is(':visible')){
+                    popup.fadeOut(200);
+                } else{
+                    // Hide other popups.
+                    $('.popup').hide();
+
+                    var width = el.outerWidth();
+                    var height = el.outerHeight();
+                    var position = el.position();
+                    var top = position.top + height;
+                    var left = position.left + width / 2;
+
+                    popup.css('top', top + 'px');
+                    popup.css('left', left + 'px');
+                    popup.fadeIn(100);
+                }
+            }
+        });
+        $('html').on('click', function(event){
+            if(($.inArray('popup', event.target.classList) > -1 || 
+                $(event.target).parents('.popup').length >= 1) && $.inArray('popup-close', event.target.classList) <= -1){
+                // Nothing to do.
+            } else{
+                $('.popup').fadeOut(200);
+            }
         });
     });
 })(window, jQuery);
